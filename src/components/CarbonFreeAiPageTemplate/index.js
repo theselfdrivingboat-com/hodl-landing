7bb8f65b68a5afb35f5750749789e1b0bcef5ef5
Siddharth Sharma <ssiddharth2010@icloud.com>
2021-07-03 21:53:54 +0100

    developers api docs -> developer sdk / docs

--- src/components/CarbonFreeAiPageTemplate/index.js
+++ src/components/CarbonFreeAiPageTemplate/index.js
@@ -1,41 +1,55 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import Testimonials from '../Testimonials'
-import ProgressiveImageContainer from '../ProgressiveImageContainer'
-import PropTypes from 'prop-types'
-import { OutboundLink } from 'gatsby-plugin-amplitude-analytics'
+import React from "react";
+import Helmet from "react-helmet";
+import Testimonials from "../Testimonials";
+import ProgressiveImageContainer from "../ProgressiveImageContainer";
+import PropTypes from "prop-types";
+import { OutboundLink } from "gatsby-plugin-amplitude-analytics";
 
 const CarbonFreeAiTrainingTemplate = (props) => {
-  const { title, heading, description, offerings, meta_title, meta_description, testimonials } = props
+  const {
+    title,
+    heading,
+    description,
+    offerings,
+    meta_title,
+    meta_description,
+    testimonials,
+  } = props;
 
   return (
     <div>
       <Helmet>
         <title>{meta_title}</title>
-        <meta name='description' content={meta_description} />
+        <meta name="description" content={meta_description} />
       </Helmet>
-      <section className='hero is-primary is-bold is-large has-background'>
-        <img className='hero-background' src='img/hodlhero.png' />
-        <div className='hero-body pt-6' style={{ paddingBottom: '15rem' }}>
-          <div className='container'>
-            <div className='columns'>
-              <div className='column is-10 is-offset-1'>
-                <div className='section pt-6 has-text-centered' style={{ paddingBottom: '15rem' }}>
-                  <h1 className='title is-1'>
-                    {title}
-                  </h1>
-                  <h2 className='subtitle is-3'>
-                    {description}
-                  </h2>
-                  <div className='columns is-centered'>
-                    <div className='column is-one-fifth'>
-                      <a href='https://docs.google.com/forms/d/e/1FAIpQLSf5a5-tKhH25EkxLizDFlX9m_bprySjY0uEE7lG4lnOfx_DtA/viewform?usp=sf_link'>
-                        <button className='button is-medium is-primary'>WAITING LIST</button>
+      <section className="hero is-primary is-bold is-large has-background">
+        <img className="hero-background" src="img/hodlhero.png" />
+        <div className="hero-body pt-6" style={{ paddingBottom: "15rem" }}>
+          <div className="container">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <div
+                  className="section pt-6 has-text-centered"
+                  style={{ paddingBottom: "15rem" }}
+                >
+                  <h1 className="title is-1">{title}</h1>
+                  <h2 className="subtitle is-3">{description}</h2>
+                  <div className="columns is-centered">
+                    <div className="column is-one-fifth">
+                      <a href="https://docs.google.com/forms/d/e/1FAIpQLSf5a5-tKhH25EkxLizDFlX9m_bprySjY0uEE7lG4lnOfx_DtA/viewform?usp=sf_link">
+                        <button className="button is-medium is-primary">
+                          WAITING LIST
+                        </button>
                       </a>
                     </div>
-                    <div className='column is-one-fifth'>
-                      <a href='https://docs.google.com/forms/d/e/1FAIpQLSf5a5-tKhH25EkxLizDFlX9m_bprySjY0uEE7lG4lnOfx_DtA/viewform?usp=sf_link'>
-                        <button className='button is-medium is-primary' disabled>DEVELOPERS API DOCS</button>
+                    <div className="column is-one-fifth">
+                      <a href="https://docs.google.com/forms/d/e/1FAIpQLSf5a5-tKhH25EkxLizDFlX9m_bprySjY0uEE7lG4lnOfx_DtA/viewform?usp=sf_link">
+                        <button
+                          className="button is-medium is-primary"
+                          disabled
+                        >
+                          DEVELOPER SDK / DOCS
+                        </button>
                       </a>
                     </div>
                   </div>
@@ -45,101 +59,75 @@ const CarbonFreeAiTrainingTemplate = (props) => {
           </div>
         </div>
       </section>
-      <section className='section section--gradient'>
-        <div className='container'>
-
-          <div className='section pt-0'>
-            <div className='columns'>
-              <div className='column is-10 is-offset-1'>
-                <div className='content has-text-centered'>
-                  <div className='columns is-multiline'>
-                    <div className='column is-12' style={{ borderRadius: '5px' }}>
-                      <p className='has-text-centered'>
-                        <ProgressiveImageContainer
-                          image='img/hodl-ai-1.png'
-                        />
-                        <OutboundLink href='/about'>
-                          <ProgressiveImageContainer
-                            image='img/hodl-ai-link-1.png'
-                          />
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="section pt-0">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <div className="content has-text-centered">
+                  <div className="columns is-multiline">
+                    <div
+                      className="column is-12"
+                      style={{ borderRadius: "5px" }}
+                    >
+                      <p className="has-text-centered">
+                        <ProgressiveImageContainer image="img/hodl-ai-1.png" />
+                        <OutboundLink href="/about">
+                          <ProgressiveImageContainer image="img/hodl-ai-link-1.png" />
                         </OutboundLink>
-                        <ProgressiveImageContainer
-                          image='img/hodl-ai-2.png'
-                        />
-                        <a href='http://www.clickclean.org/usa/en/'>
-                          <ProgressiveImageContainer
-                            image='img/hodl-ai-link-2.png'
-                          />
+                        <ProgressiveImageContainer image="img/hodl-ai-2.png" />
+                        <a href="http://www.clickclean.org/usa/en/">
+                          <ProgressiveImageContainer image="img/hodl-ai-link-2.png" />
                         </a>
-                        <ProgressiveImageContainer
-                          image='img/hodl-ai-3.png'
-                        />
+                        <ProgressiveImageContainer image="img/hodl-ai-3.png" />
                         <a href="https://cloud.google.com/sustainability">
-                        <ProgressiveImageContainer
-                          image='img/hodl-ai-link-3.png'
-                        />
+                          <ProgressiveImageContainer image="img/hodl-ai-link-3.png" />
                         </a>
-                        <ProgressiveImageContainer
-                          image='img/hodl-ai-4.png'
-                        />
+                        <ProgressiveImageContainer image="img/hodl-ai-4.png" />
                         <a href="https://arxiv.org/abs/1906.02243">
-                        <ProgressiveImageContainer
-                          image='img/hodl-ai-link-4.png'
-                        />
+                          <ProgressiveImageContainer image="img/hodl-ai-link-4.png" />
                         </a>
-                        <ProgressiveImageContainer
-                          image='img/hodl-ai-6.png'
-                        />
+                        <ProgressiveImageContainer image="img/hodl-ai-6.png" />
                         <a href="https://openai.com/blog/ai-and-compute/">
-                        <ProgressiveImageContainer
-                          image='img/hodl-ai-link-6.png'
-                        />
+                          <ProgressiveImageContainer image="img/hodl-ai-link-6.png" />
                         </a>
-                        <ProgressiveImageContainer
-                          image='img/hodl-ai-7.png'
-                        />
+                        <ProgressiveImageContainer image="img/hodl-ai-7.png" />
                       </p>
-	  <a href="https://docs.google.com/forms/d/e/1FAIpQLSf5a5-tKhH25EkxLizDFlX9m_bprySjY0uEE7lG4lnOfx_DtA/viewform?usp=sf_link">
-                        <ProgressiveImageContainer
-                          image='img/hodl-link-beta.png' />
-	  </a>
-                        <ProgressiveImageContainer
-                          image='img/hodl4.png'
-                        />
-                        <iframe style={{ width: '100%', height: '70vh' }} src='https://www.youtube.com/embed/gl36tKSPEgM' title='YouTube video player' frameBorder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen />
-                        <ProgressiveImageContainer
-                          image='img/hodl-7.png'
-                        />
-                        <div className='columns'>
-                          <a href='https://www.lessand.ro'>
-                            <div className='column'>
-                              <ProgressiveImageContainer
-                                image='img/hodl-alex.png'
-                              />
-                            </div>
-                          </a>
-                          <a href='https://twitter.com/0xflores'>
-                            <div className='column'>
-                              <ProgressiveImageContainer
-                                image='img/hodl-oana.png'
-                              />
-                            </div>
-                          </a>
-                          <a href='https://twitter.com/sid_hypeman'>
-                            <div className='column'>
-                              <ProgressiveImageContainer
-                                image='img/hodl-siddarth.png'
-                              />
-                            </div>
-                          </a>
-                          <a href='https://www.linkedin.com/in/arwa-arif/'>
-                            <div className='column'>
-                              <ProgressiveImageContainer
-                                image='img/hodl-arwa.png'
-                              />
-                            </div>
-                          </a>
-                        </div>
+                      <a href="https://docs.google.com/forms/d/e/1FAIpQLSf5a5-tKhH25EkxLizDFlX9m_bprySjY0uEE7lG4lnOfx_DtA/viewform?usp=sf_link">
+                        <ProgressiveImageContainer image="img/hodl-link-beta.png" />
+                      </a>
+                      <ProgressiveImageContainer image="img/hodl4.png" />
+                      <iframe
+                        style={{ width: "100%", height: "70vh" }}
+                        src="https://www.youtube.com/embed/gl36tKSPEgM"
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                      />
+                      <ProgressiveImageContainer image="img/hodl-7.png" />
+                      <div className="columns">
+                        <a href="https://www.lessand.ro">
+                          <div className="column">
+                            <ProgressiveImageContainer image="img/hodl-alex.png" />
+                          </div>
+                        </a>
+                        <a href="https://twitter.com/0xflores">
+                          <div className="column">
+                            <ProgressiveImageContainer image="img/hodl-oana.png" />
+                          </div>
+                        </a>
+                        <a href="https://twitter.com/sid_hypeman">
+                          <div className="column">
+                            <ProgressiveImageContainer image="img/hodl-siddarth.png" />
+                          </div>
+                        </a>
+                        <a href="https://www.linkedin.com/in/arwa-arif/">
+                          <div className="column">
+                            <ProgressiveImageContainer image="img/hodl-arwa.png" />
+                          </div>
+                        </a>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -149,8 +137,8 @@ const CarbonFreeAiTrainingTemplate = (props) => {
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 CarbonFreeAiTrainingTemplate.propTypes = {
   title: PropTypes.string,
   meta_title: PropTypes.string,
@@ -161,7 +149,6 @@ CarbonFreeAiTrainingTemplate.propTypes = {
     blurbs: PropTypes.array,
   }),
   testimonials: PropTypes.array,
+};
 
-}
-
-export default CarbonFreeAiTrainingTemplate
+export default CarbonFreeAiTrainingTemplate;
--- src/components/HomePageTemplate/index.js
+++ src/components/HomePageTemplate/index.js
@@ -1,40 +1,54 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import Testimonials from '../Testimonials'
-import ProgressiveImageContainer from '../ProgressiveImageContainer'
-import PropTypes from 'prop-types'
+import React from "react";
+import Helmet from "react-helmet";
+import Testimonials from "../Testimonials";
+import ProgressiveImageContainer from "../ProgressiveImageContainer";
+import PropTypes from "prop-types";
 
 const HomePageTemplate = (props) => {
-  const { title, heading, description, offerings, meta_title, meta_description, testimonials } = props
+  const {
+    title,
+    heading,
+    description,
+    offerings,
+    meta_title,
+    meta_description,
+    testimonials,
+  } = props;
 
   return (
     <div>
       <Helmet>
         <title>{meta_title}</title>
-        <meta name='description' content={meta_description} />
+        <meta name="description" content={meta_description} />
       </Helmet>
-      <section className='hero is-primary is-bold is-large has-background'>
-        <img className='hero-background' src='img/hodlhero.png' />
-        <div className='hero-body pt-6' style={{ paddingBottom: '15rem' }}>
-          <div className='container'>
-            <div className='columns'>
-              <div className='column is-10 is-offset-1'>
-                <div className='section pt-6 has-text-centered' style={{ paddingBottom: '15rem' }}>
-                  <h1 className='title is-1'>
-                    {title}
-                  </h1>
-                  <h2 className='subtitle is-3'>
-                    {description}
-                  </h2>
-                  <div className='columns is-centered'>
-                    <div className='column is-one-fifth'>
-                      <a href='https://docs.google.com/forms/d/e/1FAIpQLSf5a5-tKhH25EkxLizDFlX9m_bprySjY0uEE7lG4lnOfx_DtA/viewform?usp=sf_link'>
-                        <button className='button is-medium is-primary'>WAITING LIST</button>
+      <section className="hero is-primary is-bold is-large has-background">
+        <img className="hero-background" src="img/hodlhero.png" />
+        <div className="hero-body pt-6" style={{ paddingBottom: "15rem" }}>
+          <div className="container">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <div
+                  className="section pt-6 has-text-centered"
+                  style={{ paddingBottom: "15rem" }}
+                >
+                  <h1 className="title is-1">{title}</h1>
+                  <h2 className="subtitle is-3">{description}</h2>
+                  <div className="columns is-centered">
+                    <div className="column is-one-fifth">
+                      <a href="https://docs.google.com/forms/d/e/1FAIpQLSf5a5-tKhH25EkxLizDFlX9m_bprySjY0uEE7lG4lnOfx_DtA/viewform?usp=sf_link">
+                        <button className="button is-medium is-primary">
+                          WAITING LIST
+                        </button>
                       </a>
                     </div>
-                    <div className='column is-one-fifth'>
-                      <a href='https://docs.google.com/forms/d/e/1FAIpQLSf5a5-tKhH25EkxLizDFlX9m_bprySjY0uEE7lG4lnOfx_DtA/viewform?usp=sf_link'>
-                        <button className='button is-medium is-primary' disabled>DEVELOPERS API DOCS</button>
+                    <div className="column is-one-fifth">
+                      <a href="https://docs.google.com/forms/d/e/1FAIpQLSf5a5-tKhH25EkxLizDFlX9m_bprySjY0uEE7lG4lnOfx_DtA/viewform?usp=sf_link">
+                        <button
+                          className="button is-medium is-primary"
+                          disabled
+                        >
+                          DEVELOPER SDK / DOCS
+                        </button>
                       </a>
                     </div>
                   </div>
@@ -44,84 +58,66 @@ const HomePageTemplate = (props) => {
           </div>
         </div>
       </section>
-      <section className='section section--gradient'>
-        <div className='container'>
-
-          <div className='section pt-0'>
-            <div className='columns'>
-              <div className='column is-10 is-offset-1'>
-                <div className='content has-text-centered'>
-                  <div className='columns is-multiline'>
-                    <div className='column is-12' style={{ borderRadius: '5px' }}>
-                      <p className='has-text-centered'>
-                        <ProgressiveImageContainer
-                          image='img/hodl1.png'
-                        />
-                        <ProgressiveImageContainer
-                          image='img/hodl2.png'
-                        />
-                        <ProgressiveImageContainer
-                          image='img/hodl-drawing1.png'
-                        />
-                        <ProgressiveImageContainer
-                          image='img/hodl3.png'
-                        />
-                        <a href='/carbon-free-ai-training'>
-                          <ProgressiveImageContainer
-                            image='img/hodl-link-ai1.png'
-                          />
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="section pt-0">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <div className="content has-text-centered">
+                  <div className="columns is-multiline">
+                    <div
+                      className="column is-12"
+                      style={{ borderRadius: "5px" }}
+                    >
+                      <p className="has-text-centered">
+                        <ProgressiveImageContainer image="img/hodl1.png" />
+                        <ProgressiveImageContainer image="img/hodl2.png" />
+                        <ProgressiveImageContainer image="img/hodl-drawing1.png" />
+                        <ProgressiveImageContainer image="img/hodl3.png" />
+                        <a href="/carbon-free-ai-training">
+                          <ProgressiveImageContainer image="img/hodl-link-ai1.png" />
                         </a>
-                        <ProgressiveImageContainer
-                          image='img/hodl4.png'
+                        <ProgressiveImageContainer image="img/hodl4.png" />
+                        <iframe
+                          style={{ width: "100%", height: "70vh" }}
+                          src="https://www.youtube.com/embed/gl36tKSPEgM"
+                          title="YouTube video player"
+                          frameBorder="0"
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                          allowFullScreen
                         />
-                        <iframe style={{ width: '100%', height: '70vh' }} src='https://www.youtube.com/embed/gl36tKSPEgM' title='YouTube video player' frameBorder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen />
-                        <ProgressiveImageContainer
-                          image='img/hodl-7.png'
-                        />
-                        <div className='columns'>
-                          <a href='https://www.lessand.ro'>
-                            <div className='column'>
-                              <ProgressiveImageContainer
-                                image='img/hodl-alex.png'
-                              />
+                        <ProgressiveImageContainer image="img/hodl-7.png" />
+                        <div className="columns">
+                          <a href="https://www.lessand.ro">
+                            <div className="column">
+                              <ProgressiveImageContainer image="img/hodl-alex.png" />
                             </div>
                           </a>
-                          <a href='https://twitter.com/0xflores'>
-                            <div className='column'>
-                              <ProgressiveImageContainer
-                                image='img/hodl-oana.png'
-                              />
+                          <a href="https://twitter.com/0xflores">
+                            <div className="column">
+                              <ProgressiveImageContainer image="img/hodl-oana.png" />
                             </div>
                           </a>
-                          <a href='https://twitter.com/sid_hypeman'>
-                            <div className='column'>
-                              <ProgressiveImageContainer
-                                image='img/hodl-siddarth.png'
-                              />
+                          <a href="https://twitter.com/sid_hypeman">
+                            <div className="column">
+                              <ProgressiveImageContainer image="img/hodl-siddarth.png" />
                             </div>
                           </a>
-                          <a href='https://www.linkedin.com/in/arwa-arif/'>
-                            <div className='column'>
-                              <ProgressiveImageContainer
-                                image='img/hodl-arwa.png'
-                              />
+                          <a href="https://www.linkedin.com/in/arwa-arif/">
+                            <div className="column">
+                              <ProgressiveImageContainer image="img/hodl-arwa.png" />
                             </div>
                           </a>
                         </div>
-                        <ProgressiveImageContainer
-                          image='img/hodl-6.png' />
-	  <a href="https://docs.google.com/forms/d/e/1FAIpQLSf5a5-tKhH25EkxLizDFlX9m_bprySjY0uEE7lG4lnOfx_DtA/viewform?usp=sf_link">
-                        <ProgressiveImageContainer
-                          image='img/hodl-link-beta.png' />
-	  </a>
-                        <ProgressiveImageContainer
-                          image='img/hodl-8.png' />
-	  <a href="https://discord.com/invite/dUFVR9dFRZ">
-                        <ProgressiveImageContainer
-                          image='img/hodl-link-discord.png' />
-	  </a>
-                        <ProgressiveImageContainer
-                          image='img/hodl-9.png' />
+                        <ProgressiveImageContainer image="img/hodl-6.png" />
+                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSf5a5-tKhH25EkxLizDFlX9m_bprySjY0uEE7lG4lnOfx_DtA/viewform?usp=sf_link">
+                          <ProgressiveImageContainer image="img/hodl-link-beta.png" />
+                        </a>
+                        <ProgressiveImageContainer image="img/hodl-8.png" />
+                        <a href="https://discord.com/invite/dUFVR9dFRZ">
+                          <ProgressiveImageContainer image="img/hodl-link-discord.png" />
+                        </a>
+                        <ProgressiveImageContainer image="img/hodl-9.png" />
                       </p>
                     </div>
                   </div>
@@ -132,8 +128,8 @@ const HomePageTemplate = (props) => {
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 HomePageTemplate.propTypes = {
   title: PropTypes.string,
   meta_title: PropTypes.string,
@@ -144,7 +140,6 @@ HomePageTemplate.propTypes = {
     blurbs: PropTypes.array,
   }),
   testimonials: PropTypes.array,
+};
 
-}
-
-export default HomePageTemplate
+export default HomePageTemplate;
